84f23e13c56e3891e48e6f186546b158327dd9f6
jambis <imjaamees@gmail.com>
2020-04-09 15:14:36 -0500

    Added personal links and styling

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,8 @@
 import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import DescriptionIcon from "@material-ui/icons/Description";
+import EmailIcon from "@material-ui/icons/Email";
 
 import ProjectList from "./ProjectList";
 
@@ -11,6 +15,40 @@ const Main = () => {
         <div className="Header-text">
           <h2>Hey it's James Bishop</h2>
           <p>This is a introduction of me</p>
+          <div className="Header-links">
+            <a
+              href="https://github.com/jambis"
+              title="James' Github"
+              target="_blank"
+              rel="nofollow nooopener noreferr"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/jambis/"
+              title="James' Linkedin Profile"
+              target="_blank"
+              rel="nofollow nooopener noreferr"
+            >
+              <LinkedInIcon />
+            </a>
+            <a
+              href="https://drive.google.com/file/d/1EBVJZTfDBGRvHPvxuMoc-YiMVJGuUFf1/view"
+              title="James' Resume"
+              target="_blank"
+              rel="nofollow nooopener noreferr"
+            >
+              <DescriptionIcon />
+            </a>
+            <a
+              href="mailto:jdbishop22@gmail.com"
+              title="James' Email"
+              target="_blank"
+              rel="nofollow nooopener noreferr"
+            >
+              <EmailIcon />
+            </a>
+          </div>
         </div>
         <img src="https://via.placeholder.com/300x400" alt="James' Profile" />
       </div>
