4c484e26d8dfea4d979c67efb0c220067e348c4e
jambis <imjaamees@gmail.com>
2020-04-13 22:12:59 -0500

    Added link to create post

--- src/Components/Blog/Admin.js
+++ src/Components/Blog/Admin.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Admin = () => {
-  return <>Admin</>;
+  return (
+    <div>
+      <Link to="/admin/createpost">Create a new blog post</Link>
+    </div>
+  );
 };
 
 export default Admin;
