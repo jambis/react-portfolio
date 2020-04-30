14902ec2bf5ce1430091fd088a82f3beed50b106
jambis <imjaamees@gmail.com>
2020-04-30 17:08:29 -0500

    Modified the link, and removed delete button

--- src/Components/Blog/PostCard.js
+++ src/Components/Blog/PostCard.js
@@ -1,28 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { firestore } from "../../Utils/firebase";
-
-const PostCard = ({ data }) => {
-  const handleRemove = async () => {
-    await firestore.doc(`posts/${data.id}`).delete();
-  };
+const PostCard = (props) => {
+  const { title, content, slug } = props.data;
 
   return (
     <div className="PostCard">
-      <p>Title - {data.title}</p>
-      <p>Content - {data.content}</p>
-      <button type="button" onClick={handleRemove}>
-        Delete
-      </button>
-      <Link
-        to={{
-          pathname: `blog/${data.slug}`,
-          state: { data: data },
-        }}
-      >
-        Read more...
-      </Link>
+      <p>Title - {title}</p>
+      <p>Content - {content}</p>
+      <Link to={`blog/${slug}`}>Read more...</Link>
     </div>
   );
 };
