4bdd68de40d2294b678f247fdd13e9ebd9c7c574
jambis <imjaamees@gmail.com>
2020-04-30 17:06:22 -0500

    Changed to retrieve post info from context

--- src/Components/Blog/ViewPost.js
+++ src/Components/Blog/ViewPost.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+
+import { PostsContextState } from "../../Providers/PostsProvider";
 
 const ViewPost = (props) => {
-  const { data } = props.location.state;
+  const { posts } = useContext(PostsContextState);
+  const { slug } = props.match.params;
 
-  console.log(data);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    posts.some((post, i) => {
+      if (post.slug === slug) {
+        setData({
+          title: post.title,
+          date: post.date,
+          content: post.content,
+        });
+        return true;
+      } else if (post.slug !== slug && i === posts.length - 1) {
+        console.log("redirect to 404");
+      }
+    });
+  }, [slug, posts]);
+
   return (
     <div className="ViewPost">
-      <h2>{data.title}</h2>
-      <p>{data.date}</p>
-      <p>{data.content}</p>
+      <h2>{data?.title}</h2>
+      <p>{data?.date}</p>
+      <p>{data?.content}</p>
     </div>
   );
 };
