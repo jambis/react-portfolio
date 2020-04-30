8d48f2e1f9ad13b8131e909e0a5bd38533b08e33
jambis <imjaamees@gmail.com>
2020-04-30 17:04:10 -0500

    Added icons and some functionality

--- src/Components/Blog/AdminPost.js
+++ src/Components/Blog/AdminPost.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+
+import { PostsContextDispatch } from "../../Providers/PostsProvider";
+import { firestore } from "../../Utils/firebase";
 
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -6,27 +9,68 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
-import CommentIcon from "@material-ui/icons/Comment";
+import EditRoundedIcon from "@material-ui/icons/EditRounded";
+import DeleteRoundedIcon from "@material-ui/icons/DeleteRounded";
+import VisibilityRoundedIcon from "@material-ui/icons/VisibilityRounded";
 
 const AdminPost = (props) => {
-  const [checked, setChecked] = useState(false);
+  const dispatch = useContext(PostsContextDispatch);
+  const { checked, id, title, published } = props.data;
+  console.log(`${props.data.id}:`, props.data);
+
+  const handleToggle = () => {
+    published
+      ? dispatch({ type: "TOGGLE_POST", payload: id })
+      : dispatch({ type: "TOGGLE_DRAFT", payload: id });
+  };
+
+  const handleDelete = async () => {
+    if (published) {
+      await firestore.collection("posts").doc(id).delete();
+    } else {
+      await firestore.collection("drafts").doc(id).delete();
+    }
+  };
+
+  const handleEdit = () => {
+    //Set Editing to true
+    //redirect to EditPost Component
+  };
+
+  const handlePreview = () => {
+    //redirect to Preview Component
+  };
+
   return (
-    <ListItem
-      role={undefined}
-      dense
-      button
-      onClick={() => setChecked((state) => !state)}
-    >
+    <ListItem dense button onClick={handleToggle}>
       <ListItemIcon>
         <Checkbox edge="start" checked={checked} tabIndex={-1} disableRipple />
       </ListItemIcon>
-      <ListItemText primary={props.data.title} />
+      <ListItemText primary={title} />
       <ListItemSecondaryAction>
-        <IconButton edge="end" aria-label="comments">
-          <CommentIcon />
+        <IconButton
+          edge="end"
+          aria-label="preview"
+          title="Preview"
+          onClick={handlePreview}
+        >
+          <VisibilityRoundedIcon />
         </IconButton>
-        <IconButton edge="end" aria-label="comments">
-          <CommentIcon />
+        <IconButton
+          edge="end"
+          aria-label="edit"
+          title="Edit"
+          onClick={handleEdit}
+        >
+          <EditRoundedIcon />
+        </IconButton>
+        <IconButton
+          edge="end"
+          aria-label="delete"
+          title="Delete"
+          onClick={handleDelete}
+        >
+          <DeleteRoundedIcon />
         </IconButton>
       </ListItemSecondaryAction>
     </ListItem>
